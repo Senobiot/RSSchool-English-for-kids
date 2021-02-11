42aa9e2572bb55d6eb5de1370263fe0f9aea24c8
Aliaksandr Kulik <inter_net@tut.by>
2021-02-12 00:31:23 +0300

    refactor: rename folder

--- src/js/switcher_btn.js
+++
@@ -1,7 +0,0 @@
-const switcher = document.createElement('input');
-switcher.type = "checkbox";
-switcher.classList.add('switcher');
-document.body.appendChild(switcher);
-
-export default switcher;
-
